ef3fc79316389fe4602bc01cb5cd7c82e70f187c
Isabela <bela.reimberg@gmail.com>
2023-11-02 00:19:47 +0000

    made a menu

--- query.py
+++ query.py
@@ -1,3 +1,19 @@
 from movies import Movies
 
 movies = Movies('./movies.txt')
+
+
+input = scan("Please enter an input")
+while input.lower != "q":
+    
+    if(input == "sn"):
+
+    elif(input == "sc"):
+
+    elif(input == "list"):
+        list()
+
+    input = scan("Please enter an input")
+
+
+
